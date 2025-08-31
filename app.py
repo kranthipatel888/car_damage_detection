fa12d38cd9292809c300f6d54e5c5d025534132e
kranthipatel888 <kranthipatel888@gmail.com>
2025-08-31 12:17:37 +0530

    push stremlit app code

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from model_helper import predict
+
+st.title("Vechile Damage Detection")
+
+uploaded_file = st.file_uploader("upload the Image",type=['jpg','png'])
+
+if uploaded_file:
+    img_path='Image.jpg'
+    with open(img_path,'wb') as f:
+        f.write(uploaded_file.getbuffer())
+        st.image(uploaded_file, caption='uploaded Image',use_container_width=True)
+        prediction=predict(img_path)
+        st.info(f"predicted class:{prediction}")
